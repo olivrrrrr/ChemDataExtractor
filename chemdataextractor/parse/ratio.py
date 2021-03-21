82ea54171727fe2f2760633b5b3ff41cf924bd79
olivrrrrr <68692361+olivrrrrr@users.noreply.github.com>
2021-03-21 13:11:31 +0000

    Update ratio.py

--- chemdataextractor/parse/ratio.py
+++ chemdataextractor/parse/ratio.py
@@ -14,15 +14,16 @@ import logging
 import re
 
 
-from chemdataextractor.parse.cem import cem, chemical_label, lenient_chemical_label, solvent_name
-from ..utils import first
-from ..model import Compound, RatioOf
-from .actions import merge, join
-from .base import BaseParser
-from .elements import W, I, R, Optional, Any, OneOrMore, Not, ZeroOrMore
-from chemdataextractor.model import BaseModel, StringType, ListType, ModelType
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
-log = logging.getLogger(__name__)
+from .base import BaseParser
+from .elements import W, R, Optional
+from ..model import StringType, Compound
+from .actions import merge
+
 
 prefix = (I(u'ratio') | I(u'of') | I(u'and')).hide()
 
