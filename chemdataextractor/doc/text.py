f4431f2db14d6f65e35ddcdce103b6622e273dec
olivrrrrr <68692361+olivrrrrr@users.noreply.github.com>
2021-03-21 13:21:01 +0000

    Update text.py

--- chemdataextractor/doc/text.py
+++ chemdataextractor/doc/text.py
@@ -26,7 +26,7 @@ from ..parse.ir import IrParser
 from ..parse.mp import MpParser
 from ..parse.tg import TgParser
 from ..parse.nmr import NmrParser
-from ..parse.ratio import RoParser
+from ..parse.ratio import RatioParser
 from ..parse.uvvis import UvvisParser
 from ..nlp.lexicon import ChemLexicon
 from ..nlp.cem import CemTagger, IGNORE_PREFIX, IGNORE_SUFFIX, SPECIALS, SPLITS
@@ -267,7 +267,7 @@ class Heading(Text):
 
 class Paragraph(Text):
 
-    parsers = [CompoundParser(), ChemicalLabelParser(), NmrParser(), IrParser(), UvvisParser(), MpParser(), TgParser(), ContextParser(), RoParser()]
+    parsers = [CompoundParser(), ChemicalLabelParser(), NmrParser(), IrParser(), UvvisParser(), MpParser(), TgParser(), ContextParser(), RatioParser()]
 
     def _repr_html_(self):
         return '<p class="cde-paragraph">' + self.text + '</p>'
